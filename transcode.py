8e5a7ca7e38eb1463d2f51ce41185b7f08292655
Max Zhang <maxzhang@roblox.com>
2025-09-30 14:38:07 -0700

    only quality

--- transcode.py
+++ transcode.py
@@ -151,10 +151,10 @@ def main():
         if result.returncode != 0:
             print('Compilation failed. Using the existing transcode.exe.')
     
-    bitratesGsun = [1.0, 1.5, 2.0, 2.5, 3.0] # [1.0, 1.5, 2.0, 2.5, 10.0]
-    modes = ['cbr', 'vbr', 'quality'] # ['cbr', 'vbr', 'quality', 'fast']
+    bitratesGsun = [2.0] # [1.0, 1.5, 2.0, 2.5, 10.0]
+    modes = ['quality'] # ['cbr', 'vbr', 'quality', 'fast']
     hws = [True, False] # [True, False]
-    qualities = [0, 50, 100] # [0, 50, 100]
+    qualities = [10, 20, 30, 40] # [0, 50, 100]
     gops = [30, 90, 180] # [30, 90, 180]
     profiles = ["baseline", "main", "high"] # ["baseline", "main", "high", "constrained", "simple"]
     codecs = ["h264", "hevc"]
